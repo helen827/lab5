1c843b5138661ece703a5258f3a163cccb682815
vagrant <vagrant@precise32.(none)>
2015-02-06 22:13:51 +0000

    putting it together lab

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-
 var add = require('./routes/add')
-app.get('/add', add.addFriend);
+
 
 
 // Example route
@@ -41,6 +40,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/add', add.addFriend);
 // Example route
 // app.get('/users', user.list);
 
