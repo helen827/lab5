cedca053f74382e536c9cbd5f26b3925cc360581
vagrant <vagrant@precise32.(none)>
2015-02-06 22:56:55 +0000

    putting it together lab

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,19 @@
 var data = require("../data.json");
-var index = require ('./routes/index')
+
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	var data = require("../data.json");
+exports.addFriend = function(req, res) {
+ // Your code goes here
+ 
+ var newFriend= 
+ {
+ 'name': req.query.name,
+ 'description': req.query.description,
+ 'imageURL': 'http://lorempixel.com/400/400/people'
+ };
+ data["parents"].push(newFriend);
+ console.log(newFriend);
+ res.render('add');
+}
  }
